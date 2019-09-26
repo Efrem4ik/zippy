32a5d1765508eddb74c195da3306f66d2b7af26b
Efrem4ik <efrem4ik12@mail.ru>
2019-09-26 12:29:46 +0300

    end working on static version

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,25 @@
-$('.header__slider').slick({
+$(".header__slider").slick({
   dots: true,
   arrows: false
+});
+
+$(".video-slider__items").slick({
+  dots: false,
+  arrows: false,
+  centerMode: true,
+  variableWidth: true,
+  slidesToShow: 1
+});
+
+$('.life-team__big').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+});
+$('.life-team__small').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: '.life-team__big',
+  centerMode: true,
+  focusOnSelect: true,
 });

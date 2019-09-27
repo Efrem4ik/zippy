0466d4748a6390a3c7cda055cf26b5f88f618ed3
Efrem4ik <efrem4ik12@mail.ru>
2019-09-27 22:04:36 +0300

    added anchor links

--- app/js/main.js
+++ app/js/main.js
@@ -42,3 +42,12 @@ $('.life-team__small').slick({
     },
   ]
 });
+
+ $(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
